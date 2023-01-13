bfaf49e2891f67451fd5bb610fa536f593638e10
prabhas800 <prabhasnaik15326@gmail.com>
2023-01-13 13:03:29 +0530

    first commit

---
+++ src/My Components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h4>About</h4>
+      <p>
+      ToDo List App is a kind of app that generally used to maintain our day-to-day tasks or list everything that we have to do, with the most important tasks at the top of the list, and the least important tasks at the bottom. It is helpful in planning our daily schedules.
+      </p>
+    </div>
+  )
+}
+
+export default About
